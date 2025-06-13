d9116d6d2f2f961f2c90e3da8efadafd426fcc33
zhangyu <1838792979@qq.com>
2025-06-13 10:42:29 +0800

    'fix:修复icon和title'

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,23 @@
 const packageName = process.env.VUE_APP_PACKAGE_NAME || "HybridApp";
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+console.log(path.resolve(__dirname, './public/favicon.ico'))
 
 module.exports = {
   publicPath: process.env.VUE_APP_PUBLIC_PATH || "/",
   outputDir: packageName,
+
+  configureWebpack: {
+    plugins: [
+      new HtmlWebpackPlugin({
+        favicon: path.resolve(__dirname, './public/favicon.ico'),
+        title: '远程银行',
+        template: path.resolve(__dirname, './public/index.html'),
+      }),
+    ],
+  },
+
   devServer: {
     proxy: {
       "/api": {
